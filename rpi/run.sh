c4b56488c1f2054070cefd6807b5f533a1bf4a67
eigenco <42321684+eigenco@users.noreply.github.com>
2022-09-10 18:26:25 +0300

    Add files via upload

---
+++ rpi/run.sh
@@ -0,0 +1,11 @@
+echo 1000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+echo 1000000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+echo 1000000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_min_freq
+echo 1000000 > /sys/devices/system/cpu/cpu1/cpufreq/scaling_max_freq
+echo 1000000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_min_freq
+echo 1000000 > /sys/devices/system/cpu/cpu2/cpufreq/scaling_max_freq
+echo 1000000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_min_freq
+echo 1000000 > /sys/devices/system/cpu/cpu3/cpufreq/scaling_max_freq
+echo -1 > /proc/sys/kernel/sched_rt_runtime_us
+#echo 999999 > /proc/sys/kernel/sched_rt_runtime_us
+./pc
